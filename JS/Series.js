c12c42306700bcf74e62ebb067992a6a4bc56cf4
Atef <Medo.atef.1111@gmail.com>
2024-05-12 04:31:16 +0300

    final API

--- JS/Series.js
+++ JS/Series.js
@@ -1,31 +1,33 @@
-//API Key for the movie database
-var apikey = "37af78034f737ae890e4ffcb2e0dd466";
+var apikey = "8913688ba8c6297c6666f612a9c5409f";
 
 var data;
-function getdata() {
-  var myhttp = new XMLHttpRequest();
-  myhttp.open(
+function getData() {
+  var myHttp = new XMLHttpRequest();
+  myHttp.open(
     "GET",
-    `https://api.themoviedb.org/3/trending/tv/week?api_key=${apikey}`
+    `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`
   );
-  myhttp.send();
-  myhttp.addEventListener("load", function () {
-    if (myhttp.readyState == 4 && myhttp.status == 200) {
-      data = JSON.parse(myhttp.response);
+  myHttp.send();
+  myHttp.addEventListener("load", function () {
+    if (myHttp.readyState == 4 && myHttp.status == 200) {
+      data = JSON.parse(myHttp.response);
       var section = document.querySelector(`.row`);
       for (var i = 0; i < data.results.length; i++) {
+        const movie = data.results[i];
         const card = document.createElement("div");
         card.classList.add("col-1", "card");
-        card.innerHTML = `
-        <img src="https://image.tmdb.org/t/p/w500/${data.results[i].poster_path}">
-        <div class="overlay">
-          <div class="text">${data.results[i].title}</div>
-        </div>
-      `;
-        console.log(data.results[i].poster_path);
+        const anchor = document.createElement("a");
+        anchor.href = `/Pages/description.html?id=${movie.id}&type=movie`; // Pass movie ID in the query parameter
+        anchor.innerHTML = `
+          <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}">
+          <div class="overlay">
+            <div class="text">${movie.title}</div>
+          </div>
+        `;
+        card.appendChild(anchor);
         section.appendChild(card);
       }
     }
   });
 }
-getdata();
+getData();
