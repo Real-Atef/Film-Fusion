d5a310bd34f30492be712d34b9c24ea6b0906033
Atef <Medo.atef.1111@gmail.com>
2024-05-12 04:42:07 +0300

    Update Series.js

--- JS/Series.js
+++ JS/Series.js
@@ -1,11 +1,12 @@
-var apikey = "8913688ba8c6297c6666f612a9c5409f";
+// API Key for the movie database
+var apiKey = "8913688ba8c6297c6666f612a9c5409f";
 
 var data;
 function getData() {
   var myHttp = new XMLHttpRequest();
   myHttp.open(
     "GET",
-    `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`
+    `https://api.themoviedb.org/3/trending/tv/week?api_key=${apiKey}`
   );
   myHttp.send();
   myHttp.addEventListener("load", function () {
@@ -13,15 +14,15 @@ function getData() {
       data = JSON.parse(myHttp.response);
       var section = document.querySelector(`.row`);
       for (var i = 0; i < data.results.length; i++) {
-        const movie = data.results[i];
+        const tvShow = data.results[i];
         const card = document.createElement("div");
         card.classList.add("col-1", "card");
         const anchor = document.createElement("a");
-        anchor.href = `/Pages/description.html?id=${movie.id}&type=movie`; // Pass movie ID in the query parameter
+        anchor.href = `/Pages/description.html?id=${tvShow.id}&type=tv`; // Pass TV show ID in the query parameter
         anchor.innerHTML = `
-          <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}">
+          <img src="https://image.tmdb.org/t/p/w500/${tvShow.poster_path}">
           <div class="overlay">
-            <div class="text">${movie.title}</div>
+            <div class="text">${tvShow.name}</div>
           </div>
         `;
         card.appendChild(anchor);
